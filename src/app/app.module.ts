5e48bf6bdab475a439b8d30b2dc3c2674b39c62a
Alexandra-Duran <miguelcaicedodiaz10@gmail.com>
2024-03-17 10:48:36 -0500

    Fin de seccion 4

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { CounterModule } from './counter/counter.modele';
+
+
+import { AppComponent } from './app.component';
+import { HeroesModule } from './heroes/heroes.module';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    CounterModule,
+    HeroesModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
